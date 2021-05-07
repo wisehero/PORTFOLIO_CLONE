06d5ab2b968d0446e6c5ed8313de280544fcd5c3
김지웅 <godjw96@gmail.com>
2021-05-07 13:52:30 +0900

    home contact button

--- main.js
+++ main.js
@@ -21,7 +21,17 @@ navbarMenu.addEventListener("click", (event) => {
     return;
   }
 
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView(link);
 });
+
+// Handle contact me button
+
+const homeContactButton = document.querySelector(".home__contact");
+homeContactButton.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
